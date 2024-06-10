cebe38b3caee209dd283b37315f3519f5608e969
Vhaguu <sandipdevkota9@gmail.com>
2024-06-10 14:54:59 +0545

    finished newsportal

--- LawSathi/NewsPortal/urls.py
+++ LawSathi/NewsPortal/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from.import views
 
 urlpatterns = [
-    path('',views.landingpage,name = "landingpage"),
+    path('',views.landingpage,name = "newsportal"),
 ]
