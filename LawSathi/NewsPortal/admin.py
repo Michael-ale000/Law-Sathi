cc27bf60eee68739fa413b4de42814a63bf99a5d
Vhaguu <sandipdevkota9@gmail.com>
2024-06-16 08:23:54 +0545

    created user models

--- LawSathi/NewsPortal/admin.py
+++ LawSathi/NewsPortal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import MoreUserInfo
 
 # Register your models here.
+class MoreUserInfoInline(admin.StackedInline):
+    model = MoreUserInfo
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (MoreUserInfoInline,)
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
---
+++ LawSathi/NewsPortal/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import MoreUserInfo
+
+@receiver(post_save, sender=User)
+def create_or_update_mpreuserinfo(sender,instance,created,**kwargs):
+    if created:
+        MoreUserInfo.objects.create(user=instance)
+    else:
+        instance.profile.save()
