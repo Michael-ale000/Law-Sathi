392e974efa4fe21fcb73b66ee40a12abb61248cf
Michael-ale000 <michaelale85@gmail.com>
2024-06-17 18:56:46 +0545

    templates corrected

--- LawSathi/VideoConsultation/urls.py
+++ LawSathi/VideoConsultation/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('dashboard',views.dashboard,name='dashboard'),
+    path('dashboard/',views.dashboard,name='dashboard'),
     path('meeting/',views.videocall, name='meeting'),
     path('join/',views.join_room, name='join_room'),
 ]
