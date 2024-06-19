bfc7a151bd0abe8d979140c1ebf5a6267fb948bc
Michael-ale000 <michaelale85@gmail.com>
2024-06-19 13:48:19 +0545

    Rating is added to the database using composite score calculation

--- LawSathi/LawyerRecommendation/models.py
+++ LawSathi/LawyerRecommendation/models.py
@@ -19,6 +19,11 @@ class Address(models.Model):
     
 
 #laywaer details
+from django.db import models
+from django.contrib.auth.models import User
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
 class LawyerDetails(models.Model):
 
     STATUS_CHOICES = [
@@ -27,7 +32,7 @@ class LawyerDetails(models.Model):
         ('rejected', 'Rejected'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     experience = models.PositiveIntegerField(help_text="Years of experience")
     bar_license = models.CharField(max_length=100, unique=True)
     average_case_completion_days = models.PositiveIntegerField()
@@ -35,11 +40,52 @@ class LawyerDetails(models.Model):
     office_address = models.OneToOneField(Address, related_name='office_address', on_delete=models.CASCADE)
     is_lawyer = models.BooleanField(default=True, editable=False)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
-    
-    def __str__(self):
-        if self.user_id:
-            return f"{self.user.username}"
-        return f"LawyerDetails - {self.bar_license}"
+    Rating = models.IntegerField(default=0)
+
+    def calculate_experience_rating(self):
+        if self.experience >= 28:
+            return 5
+        elif self.experience >= 20:
+            return 4
+        elif self.experience >= 10:
+            return 3
+        elif self.experience >= 5:
+            return 2
+        else:
+            return 1
+
+    def calculate_composite_score(self):
+        experience_rating = self.calculate_experience_rating()
+        avgDaysOfCompletion_normalized = self.normalize_avgDaysOfCompletion()
+        composite_score = (0.5 * experience_rating + 
+                           0.25 * (1 - avgDaysOfCompletion_normalized))
+        return composite_score
+
+    def normalize_avgDaysOfCompletion(self):
+        all_lawyers = LawyerDetails.objects.all()
+        avg_days = np.array([lawyer.average_case_completion_days for lawyer in all_lawyers]).reshape(-1, 1)
+        scaler = MinMaxScaler()
+        scaler.fit(avg_days)
+        normalized_value = scaler.transform(np.array([[self.average_case_completion_days]]))[0][0]
+        return normalized_value
+
+    def calculate_final_rating(self):
+        composite_score = self.calculate_composite_score()
+        if composite_score >= 2.5:
+            return 5
+        elif composite_score >= 2:
+            return 4
+        elif composite_score >= 1.5:
+            return 3
+        elif composite_score >= 1:
+            return 2
+        else:
+            return 1
+
+    def save(self, *args, **kwargs):
+        self.Rating = self.calculate_final_rating()
+        super(LawyerDetails, self).save(*args, **kwargs)
+
 
 #lawyer documents
 class LawyerDocuments(models.Model):
