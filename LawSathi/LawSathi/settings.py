41a642a51786f396901f9dab469bfc41ce627753
Vhaguu <sandipdevkota9@gmail.com>
2024-08-21 10:13:40 +0545

    made custom admin pannel

--- LawSathi/LawSathi/settings.py
+++ LawSathi/LawSathi/settings.py
@@ -31,16 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Chatbot",
+    "NewsPortal",
+    "LawyerRecommendation",
+    "VideoConsultation",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Chatbot",
-    "NewsPortal",
-    "LawyerRecommendation",
-    "VideoConsultation",
     'social_django', #for gmail login
     'rest_framework',
     'corsheaders',
